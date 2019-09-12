7b9b3b3cd15303816085662c1569c6d929727d78
Will S <wsha.code@gmail.com>
2019-09-12 01:28:05 -0400

    Add a lot of simple key bindings

--- addon/content.js
+++ addon/content.js
@@ -1,4 +1,87 @@
 'use strict'
-/* global Mousetrap, console */
+/* global Mousetrap, document */
 
-Mousetrap.bind('r', function() {console.log('page_keys: r')})
+Mousetrap.bind(
+    'c',
+    function() {
+        let buttons = document.getElementsByClassName("compose-button")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    'r',
+    function() {
+        let buttons = document.getElementsByClassName("action reply")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    'a',
+    function() {
+        let buttons = document.getElementsByClassName("action reply-all")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    'F',
+    function() {
+        let buttons = document.getElementsByClassName("action forward")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    '#',
+    function() {
+        let buttons = document.getElementsByClassName("action pim-delete")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    'v',
+    function() {
+        let buttons = document.getElementsByClassName("action pim-move-to-folder")
+        for (let button of buttons) {
+            if (button.getAttribute("data-is-inactive") == "true") {
+                continue
+            }
+            button.click()
+            break
+        }
+        return false
+    }
+)
+
+Mousetrap.bind(
+    'u',
+    function() {
+        let buttons = document.getElementsByClassName("refresh-button")
+        let button = buttons[0]
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    '/',
+    function() {
+        let bar = document.getElementsByClassName("seq-search-bar")[0]
+        let search = bar.getElementsByClassName("shadow-input")[0]
+        search.focus()
+        return false
+    }
+)
