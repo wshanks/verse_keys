5c28a22064a4c59f3c1b284876d92710da3b998b
Will S <wsha.code@gmail.com>
2019-09-12 17:31:01 -0400

    TIL querySelector is good

--- addon/content.js
+++ addon/content.js
@@ -1,86 +1,76 @@
-'use strict'
-/* global Mousetrap, document */
+"use strict"
+/* global Mousetrap, window, document, MouseEvent */
 
 Mousetrap.bind(
-    'c',
+    "c",
     function() {
-        let buttons = document.getElementsByClassName("compose-button")
-        let button = buttons[0]
+        let button = document.querySelector(".compose-button")
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    'r',
+    "r",
     function() {
-        let buttons = document.getElementsByClassName("action reply")
-        let button = buttons[0]
+        let button = document.querySelector(".action.reply")
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    'a',
+    "a",
     function() {
-        let buttons = document.getElementsByClassName("action reply-all")
-        let button = buttons[0]
+        let button = document.querySelector(".action.reply-all")
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    'F',
+    "F",
     function() {
-        let buttons = document.getElementsByClassName("action forward")
-        let button = buttons[0]
+        let button = document.querySelector(".action.forward")
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    '#',
+    "#",
     function() {
-        let buttons = document.getElementsByClassName("action pim-delete")
-        let button = buttons[0]
+        let button = document.querySelector(
+            ".action.pim-delete[data-is-inactive='false'], " +
+            ".action.delete-all[data-is-inactive='false']"
+        )
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    'v',
+    "v",
     function() {
-        let buttons = document.getElementsByClassName("action pim-move-to-folder")
-        for (let button of buttons) {
-            if (button.getAttribute("data-is-inactive") == "true") {
-                continue
-            }
-            button.click()
-            break
-        }
-        return false
-    }
-)
-
-Mousetrap.bind(
-    'u',
-    function() {
-        let buttons = document.getElementsByClassName("refresh-button")
-        let button = buttons[0]
+        let button = document.querySelector(".action.pim-move-to-folder[data-is-inactive='false']")
         button.click()
         return false
     }
 )
 
 Mousetrap.bind(
-    '/',
+    "u",
     function() {
-        let bar = document.getElementsByClassName("seq-search-bar")[0]
-        let search = bar.getElementsByClassName("shadow-input")[0]
+        let button = document.querySelector(".refresh-button")
+        button.click()
+        return false
+    }
+)
+
+Mousetrap.bind(
+    "/",
+    function() {
+        let search = document.querySelector(".seq-search-bar .shadow-input")
         search.focus()
         return false
     }
@@ -88,24 +78,17 @@ Mousetrap.bind(
 
 let VerseKeys = {
     findContent: function() {
-        let container = document.getElementsByClassName("preview-container")[0]
-        let view = container.getElementsByClassName("read-view body-open")[0]
-        let contents = view.getElementsByClassName("content")
-        for (let content of contents) {
-            if (!content.classList.contains("collapsed-content")) {
-                return content
-            }
-        }
-
-        return undefined
+        let content = document.querySelector(
+            ".preview-container .read-view.body-open .content:not(.collapsed-content)")
+        return content
 
     },
 
     toggleMessageListContent: function() {
         // Return true if element focused. Otherwise false.
         let content = this.findContent()
-        if (content && content == document.activeElement) {
-            let messageList = document.getElementsByClassName("messageList")[0]
+        if (content == document.activeElement) {
+            let messageList = document.querySelector(".messageList")
             if (messageList) {
                 messageList.focus()
                 return true
@@ -116,11 +99,19 @@ let VerseKeys = {
         }
 
         return false
+    },
+
+    scrollContent: function(step) {
+        let content = this.findContent()
+        let cssHeight = window.getComputedStyle(content).
+            getPropertyValue("line-height")
+        let pixels = parseInt(cssHeight.substr(0, cssHeight.length - 2), 10)
+        content.scrollTop += step * pixels
     }
 }
 
 Mousetrap.bind(
-    'm',
+    "m",
     function() {
         if (VerseKeys.toggleMessageListContent()) {
             return false
